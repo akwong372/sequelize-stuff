631f19c315f5122dee6d10c32e29fccd3f469d05
Alvin Kwong <akwong372@ip-192-168-86-30.us-west-2.compute.internal>
2021-09-02 17:02:03 -0700

    add basic server setup

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 require('dotenv').config();
+const express = require('express');
 const { Sequelize } = require('sequelize');
+const app = express();
+const port = 3000;
+
+const sequelize = new Sequelize(process.env.DBURL);
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
